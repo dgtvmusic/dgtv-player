5519d5bf71982a08284eb30e4e79f7d2e1b4f1f6
dgtvmusic <dgtvmusic@gmail.com>
2026-07-04 13:14:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -181,7 +181,9 @@ els.search.addEventListener('input', () => {
   renderPrograms(filtered);
 });
 
-els.siteBtn.innerHTML = 'LIVE RADIO';.siteBtn.addEventListener('click', () => {
+els.siteBtn.innerHTML = 'LIVE RADIO';
+
+els.siteBtn.addEventListener('click', () => {
   window.open('https://www.dgtvmusic.com', '_blank');
 });
 
