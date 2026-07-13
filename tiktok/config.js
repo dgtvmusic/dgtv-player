2da8d3e2cb7d8a45cd8b6fb302ef8bbd555963e4
dgtvmusic <dgtvmusic@gmail.com>
2026-07-13 10:26:26 +0200

    Add files via upload

---
+++ tiktok/config.js
@@ -0,0 +1,11 @@
+// CONFIGURAZIONE DG TV LIVE RADIO
+// Sostituisci STREAM_URL con l'indirizzo diretto MP3/AAC della radio.
+// Gli altri campi sono già pronti.
+
+window.DGTV_CONFIG = {
+  STREAM_URL: "INCOLLA_QUI_URL_STREAM",
+  TITLE: "DG TV Live Radio",
+  ARTIST: "Musica non stop",
+  COVER: "assets/cover.jpg",
+  AUTOPLAY: false
+};
